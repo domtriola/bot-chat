0c3554016b2d229611a6e2cf01f6b9d260837b50
Dominick Triola <dominick@ignovadesign.com>
2017-05-09 17:48:13 -0700

    add auth plug and remove unused actions from convo controller

--- apps/chat/web/controllers/convo_controller.ex
+++ apps/chat/web/controllers/convo_controller.ex
@@ -3,15 +3,7 @@ defmodule Chat.ConvoController do
 
   alias Chat.Convo
 
-  def index(conn, _params) do
-    convos = Repo.all(Convo)
-    render(conn, "index.html", convos: convos)
-  end
-
-  def new(conn, _params) do
-    changeset = Convo.changeset(%Convo{})
-    render(conn, "new.html", changeset: changeset)
-  end
+  plug :authenticate_user when action in [:show]
 
   def create(conn, %{"convo" => convo_params}) do
     changeset = Convo.changeset(%Convo{}, convo_params)
@@ -32,12 +24,6 @@ defmodule Chat.ConvoController do
     render(conn, "show.html", convo: convo)
   end
 
-  def edit(conn, %{"id" => id}) do
-    convo = Repo.get!(Convo, id)
-    changeset = Convo.changeset(convo)
-    render(conn, "edit.html", convo: convo, changeset: changeset)
-  end
-
   def update(conn, %{"id" => id, "convo" => convo_params}) do
     convo = Repo.get!(Convo, id)
     changeset = Convo.changeset(convo, convo_params)
@@ -50,17 +36,5 @@ defmodule Chat.ConvoController do
       {:error, changeset} ->
         render(conn, "edit.html", convo: convo, changeset: changeset)
     end
-  end
-
-  def delete(conn, %{"id" => id}) do
-    convo = Repo.get!(Convo, id)
-
-    # Here we use delete! (with a bang) because we expect
-    # it to always work (and if it does not, it will raise).
-    Repo.delete!(convo)
-
-    conn
-    |> put_flash(:info, "Convo deleted successfully.")
-    |> redirect(to: convo_path(conn, :index))
   end
 end
