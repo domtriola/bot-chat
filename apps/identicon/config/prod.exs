d44ac5334d75f47686a51a76ea2d8c32514efa35
Dominick Triola <dominick@ignovadesign.com>
2017-05-11 23:20:57 -0700

    fix env access

--- apps/identicon/config/prod.exs
+++ apps/identicon/config/prod.exs
@@ -1,4 +1,4 @@
 use Mix.Config
 config :ex_aws,
-  access_key_id: [{:system, System.get_env("S3_ACCESS_KEY_ID")}, :instance_role],
-  secret_access_key: [{:system, System.get_env("S3_SECRET_ACCESS_KEY")}, :instance_role]
+  access_key_id: [{:system, "S3_ACCESS_KEY_ID"}, :instance_role],
+  secret_access_key: [{:system, "S3_SECRET_ACCESS_KEY"}, :instance_role]
